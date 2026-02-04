9484e439c24ef0e791d6ef53c5c41619b47e7170
Pradip Chaudhary <chaudharypradip678@gmail.com>
2026-02-04 14:30:33 +0545

    Delete prisma directory

--- prisma/migrations/20241222041424_update_skills/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Skill" ADD COLUMN     "iconImage" TEXT,
-ADD COLUMN     "iconImageId" TEXT,
-ADD COLUMN     "primaryColor" TEXT,
-ADD COLUMN     "secondaryColor" TEXT,
-ALTER COLUMN "isHighlighted" SET DEFAULT false;
